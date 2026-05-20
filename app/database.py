f8d415fdee3309aa474da48f6820fa00b6700248
Adelya <adelyao276@gmail.com>
2026-05-20 16:07:27 +0500

    Connect Neon database

--- app/database.py
+++ app/database.py
@@ -1,40 +1,25 @@
+import os
+
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
-from fastapi import Depends  # Импортируем Depends из fastapi
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
+# Берем DATABASE_URL из Render/Neon
+# Если её нет — используем локальную PostgreSQL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://postgres:12345@localhost:5433/dance_workshops_db"
 )
 
+# Подключение к базе данных
 engine = create_engine(DATABASE_URL)
 
+# Создание сессии
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+# Базовый класс моделей
 Base = declarative_base()
-# Создаём объект Base
-Base = declarative_base()
-
-# Создаём подключение к базе данных
-engine = create_engine(DATABASE_URL)
-
-# Создаём сессию для взаимодействия с базой
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Функция для получения сессии базы данных
-def get_db(db: Session = Depends(SessionLocal)):
-    try:
-        yield db
-    finally:
-        db.close()  # Закрывает сессию после выполнения запроса
